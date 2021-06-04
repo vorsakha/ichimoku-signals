1e8e2e9e4875c300aec5be7972c33b24a66ee645
tsaristbomba <andrexerdna@gmail.com>
2021-06-04 16:42:27 -0300

    cleaning

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 const getSignal = require("./signal");
-const getSma = require("./smaHighsLows");
-const api = require("./api");
 
 setInterval(async () => {
-  await getSignal("15m");
+  await getSignal("5m");
 }, process.env.CRAWLER_INTERVAL);
-
-/*async function test() {
-  const data = await api.klines(process.env.SYMBOL, 120 * 30, "5m");
-  const high = data.high;
-  const low = data.low;
-
-  const dutu = getSma(high, low, 120, 31);
-  console.log(dutu);
-}
-
-test();*/
