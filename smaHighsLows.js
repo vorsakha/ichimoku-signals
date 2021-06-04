1c56b748c0b7cf70d01a4d6889cd7bbec2100cd6
tsaristbomba <andrexerdna@gmail.com>
2021-06-04 16:16:56 -0300

    adjusting sma calculations the way that ichimoku requires

--- smaHighsLows.js
+++ smaHighsLows.js
@@ -6,25 +6,6 @@ function getHL(high, low, periods, start = 1) {
   const lowNumber = Math.min(...sliceLow);
 
   return (highNumber + lowNumber) / 2;
-
-  // const sliceHigh = high.slice(start, periods + 1);
-  // const sliceLow = low.slice(start, periods + 1);
-
-  // const reduceHigh = sliceHigh.reduce(
-  //   (accumulator, current) => +accumulator + +current
-  // );
-  // const resultHigh = reduceHigh / sliceHigh.length;
-
-  // const reduceLow = sliceLow.reduce(
-  //   (accumulator, current) => +accumulator + +current
-  // );
-  // const resultLow = reduceLow / sliceLow.length;
-
-  // const result = (resultHigh + resultLow) / 2;
-
-  // return result;
 }
 
 module.exports = getHL;
-
-// one actual, one 30 candles behind
